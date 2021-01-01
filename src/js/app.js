489a07287bb26a055bc73c341cac003b3ee37b0f
maxpushka <maxpushka200@gmail.com>
2021-01-01 03:09:00 +0200

    Added local storage caching

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,56 @@
 "use strict";
-const TOKEN = "";
-const payload = '&sync_token=*&resource_types=["all"]';
 
-function updateChatList() {
-	const fetchURL = `https://api.todoist.com/sync/v8/sync?${TOKEN}${payload}`;
+const BASE_URL = 'https://api.todoist.com/sync/v8/sync';
+const TOKEN = '2ab5e4aaeafd1caa19c5cc2464f7ae12341a05f1';
+const FULL_SYNC = 'sync_token=*&resource_types=[%22all%22]';
+
+function update(type) {
+  const fetchURL = `${BASE_URL}?token=${TOKEN}&${type}`;
 
 	/* Create processing element */
 	let page = document.getElementById(pageId);
 	let processingEl = document.createElement("div");
 	processingEl.setAttribute("class", "ui-processing ui-processing-full-size");
-	processingEl.setAttribute("id", "processing");
+  processingEl.setAttribute("id", "processing");
   page.append(processingEl);
-  
+
   /* Get chatlist data from server */
   fetch(fetchURL)
-    .then( response => {return response.json();} )
+    .then( response => {
+      for (let [key, value] of response.headers) {
+        console.log(`${key} = ${value}`);
+      }
+      return response.json();
+    } )
     .then( json => {
-      console.log(`Received ${json.length} objects of type '${typeof(json)}'`);
-      // const recieved = cache.concat(json.slice(0,5));
-      // page.dispatchEvent( new CustomEvent("update-list", {detail: {"JSON_DATA": recieved, "type":"chat-vlist"}}) );
+      console.log("received json: ", json);
+      for (let key in json) {
+        localStorage.setItem( key, JSON.stringify(json[key]) );
+      }
+
+      const todolist_items = JSON.parse(localStorage.getItem("items")); // string from local storage -> json
+      const inbox = todolist_items.filter(i => i.project_id == "2251947170"); // Inbox
+      console.log("inbox: ", inbox);
+      page.dispatchEvent( new CustomEvent("draw-vlist", {detail: {"JSON_DATA": inbox}}) );
     })
-    .catch( (error) => {
+    .catch( error => {
       console.log("Error: " + String(error));
       tau.openPopup("#failed-update");
     })
-    .finally( () => processingEl.remove() );
-    
+    /* Remove processing element */
+    .finally( () => {processingEl.remove();} );
+
 }
+
+function color(num) {
+ const colors = [
+  "#b8256f", "#db4035", "#ff9933", "#fad000", "#afb83b",
+  "#7ecc49", "#299438", "#6accbc", "#158fad", "#14aaf5",
+  "#96c3eb", "#4073ff", "#884dff", "#af38eb", "#eb96eb",
+  "#e05194", "#ff8d85", "#808080", "#b8b8b8", "#ccac93"
+ ];
+
+ return colors[num-30];
+}
+
+update(FULL_SYNC);
--- src/js/vlist.js
+++ src/js/vlist.js
@@ -7,7 +7,7 @@
 		listHelper;
 
 	// Do preparatory works and adds event listeners
-	pageElement.addEventListener("draw-list", function (event) {
+	pageElement.addEventListener("draw-vlist", function (event) {
 		/* Get HTML element reference */
 		let template = document.getElementById(templateId).innerHTML,
 			JSON_DATA = event.detail.JSON_DATA,
@@ -19,30 +19,13 @@
 			options.snap = {animate: "scale"};
 		}
 
-		/* Add "New message" button to top of the list */
-		console.log(`payload: ${event.detail.type} ${listId}`);
-		if (event.detail.type == listId) {
-			for (let key of ["new-msg", "saved-msgs"]) {
-				let li = document.createElement("li");
-				li.setAttribute("class", "ui-li-grid");
-
-				let str = document.getElementById(key).innerHTML;
-				let parser = new DOMParser();
-				let newEl = parser.parseFromString(str, "text/html").body.childNodes;
-
-				for (let i = 0; i < newEl.length; i++) {li.append(newEl[i]);}
-				listElement.append(li);
-				console.log(`appended length: ${newEl.length} ${newEl}`);
-			}
-		}
-
 		virtualListWidget = tau.widget.Listview(listElement, options);
 
 		/* Update list items */
 		virtualListWidget.setListItemUpdater(function (li, newIndex) {
 			let data = JSON_DATA[newIndex];
 
-			li.innerHTML = template.replace(/\$\{([\w]+)\}/ig,
+			li.innerHTML = template.replace(/\$\{([\w.]+)\}/ig,
 				(pattern, field) => parser(pattern, field, data) );
 
 			if (itemClass.length) {
@@ -57,19 +40,28 @@
 	});
 
 	// Destroys and removes event listeners
-	pageElement.addEventListener("destroy-list", function () {
+	pageElement.addEventListener("destroy-vlist", function () {
 		virtualListWidget.destroy(); // remove all children in the virtual list
 		if (listHelper) listHelper.destroy();
 		console.log("destroyed");
 	});
 
-	pageElement.addEventListener("update-list", function (event) {
-		pageElement.dispatchEvent( new CustomEvent("destroy-list") );
-		pageElement.dispatchEvent( new CustomEvent("draw-list", {detail: {"JSON_DATA":event.detail.JSON_DATA, "type":"chat-vlist"}}) );
+	pageElement.addEventListener("update-vlist", function (event) {
+		pageElement.dispatchEvent( new CustomEvent("destroy-vlist") );
+		pageElement.dispatchEvent( new CustomEvent("draw-vlist", {detail: {"JSON_DATA":event.detail.JSON_DATA}}) );
 		console.log("updated");
 	});
 })(pageId, listId, templateId, itemClass);
 
-let li_node = {}
+const SVG = {
+	recurring: `<svg width="24" height="24" viewBox="0 0 12 12" class="recurring_icon"><path fill="currentColor" d="M2.784 4.589l.07.057 1.5 1.5a.5.5 0 01-.638.765l-.07-.057L3 6.207V7a2 2 0 001.85 1.995L5 9h2.5a.5.5 0 01.09.992L7.5 10H5a3 3 0 01-2.995-2.824L2 7v-.793l-.646.647a.5.5 0 01-.638.057l-.07-.057a.5.5 0 01-.057-.638l.057-.07 1.5-1.5a.5.5 0 01.638-.057zM7 2a3 3 0 013 3v.792l.646-.646a.5.5 0 01.765.638l-.057.07-1.5 1.5a.5.5 0 01-.638.057l-.07-.057-1.5-1.5a.5.5 0 01.638-.765l.07.057.646.646V5a2 2 0 00-1.85-1.995L7 3H4.5a.5.5 0 010-1z"></path></svg>`,
+}
 
-function parser(pattern, field, data) {}
+function parser(pattern, field, data) {
+	// console.log("newIndex:", data, field);
+	let obj = data;
+	field.split('.').forEach( (i) => {obj = obj[i];} );
+
+	if (field === "due.is_recurring" && obj == true) {return SVG.recurring;}
+	else {return obj;}
+}
